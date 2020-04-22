256da8b340810e244dfdce150b113c281c909f02
Noah <51218002+ungenickt-de@users.noreply.github.com>
2020-04-22 15:30:58 +0200

    Add files via upload

---
+++ System Tester.bat
@@ -0,0 +1,20 @@
+@echo off
+echo Willkommen beim System Tester by Noah DeV
+echo Es wird Ausgeführt: systeminfo 
+echo Drücke eine Taste um das System zu Testen
+pause
+color 2
+systeminfo
+color 3
+echo Fertig! Danke fürs nutzen. Noah  DeV 
+echo https://github.com/ungenickt-de/
+color c
+pause
+
+############################################
+#                                          #
+#                                          #
+# Noah DeV System Tester v1.0.0 COPYRIGHT! #
+#                                          #
+#                                          #
+############################################
